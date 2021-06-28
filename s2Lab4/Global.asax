4a132baf48ff3e6ebaa1e50427458008444f0cf5
nguyenthanhtunghutechsg <nguyenthanhtunghutechsg@gmail.com>
2021-06-28 07:58:32 +0700

    Add project files.

---
+++ s2Lab4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="s2Lab4.MvcApplication" Language="C#" %>
---
+++ s2Lab4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(s2Lab4.Startup))]
+namespace s2Lab4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
