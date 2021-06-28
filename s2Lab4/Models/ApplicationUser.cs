970f81d1782d825eecf7d014c02ed3460d7ebe24
nguyenthanhtunghutechsg <nguyenthanhtunghutechsg@gmail.com>
2021-06-28 08:18:30 +0700

    them cac file migration

--- s2Lab4/Models/ApplicationUser.cs
+++ s2Lab4/Models/ApplicationUser.cs
@@ -18,16 +18,5 @@ namespace s2Lab4.Models
         }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-    }
+    
 }
---
+++ s2Lab4/Models/Course.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace s2Lab4.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        public ApplicationUser Lecturer { get; set; }
+        [Required]
+        public string LectureId { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Place { get; set; }
+
+        public DateTime date { get; set; }
+        [Required]
+        public Category category { get; set; }
+
+        public byte CategoryId { get; set; }
+
+    }
+   
+}
